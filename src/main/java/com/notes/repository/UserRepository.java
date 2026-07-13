5eb60ac41688a1f3d3c22aa2c0d2524b22130af8
Srivalasalan <manikandan85334@gmail.com>
2026-07-13 12:59:40 +0530

    Inherits JpaRepository

--- src/main/java/com/notes/repository/UserRepository.java
+++ src/main/java/com/notes/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.notes.repository;
 
 import com.notes.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<Users,Long> {
     
     Optional<Users> findByUsername(String username);
     Optional<Users> findByEmail(String email);
