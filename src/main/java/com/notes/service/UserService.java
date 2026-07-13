691d4c37029f94b2a4559566ac325a620ab73f9e
Srivalasalan <manikandan85334@gmail.com>
2026-07-13 15:16:59 +0530

    Implemented Service

---
+++ src/main/java/com/notes/service/UserService.java
@@ -0,0 +1,7 @@
+package com.notes.service;
+
+import com.notes.dto.RegisterRequest;
+
+public interface UserService  {
+    void register(RegisterRequest request);
+}
---
+++ src/main/java/com/notes/service/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.notes.service;
+
+import com.notes.dto.RegisterRequest;
+import com.notes.entity.Users;
+import com.notes.repository.UserRepository;
+import jakarta.persistence.EntityExistsException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+    private final UserRepository repo;
+    private final PasswordEncoder encoder;
+    @Override
+    public void register( RegisterRequest request) {
+        if(repo.existsByUsername(request.username())){
+            throw new EntityExistsException("Username Already Exists");
+        }
+        if(repo.existsByEmail(request.email())){
+            throw new EntityExistsException("Email Already Exists");
+        }
+        Users users=Users.builder()
+                .username(request.username())
+                .email(request.email())
+                .password(encoder.encode(request.password()))
+                .build();
+
+        repo.save(users);
+    }
+}
