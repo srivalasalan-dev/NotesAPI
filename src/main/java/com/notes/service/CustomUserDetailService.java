d6981d894da2879bdd9b1314467a3ddb7993e510
Srivalasalan <manikandan85334@gmail.com>
2026-07-13 12:31:45 +0530

    Implemented UserDetailService

--- src/main/java/com/notes/service/CustomUserDetailService.java
+++ src/main/java/com/notes/service/CustomUserDetailService.java
@@ -1,4 +1,29 @@
 package com.notes.service;
 
-public class CustomUserDetailService {
+import com.notes.entity.Users;
+import com.notes.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       Users user= repo.findByUsername(username)
+               .orElseThrow(()-> new UsernameNotFoundException("Username Not Found!"));
+
+       return User.builder()
+               .username(user.getUsername())
+               .password(user.getPassword())
+               .build();
+
+    }
 }
