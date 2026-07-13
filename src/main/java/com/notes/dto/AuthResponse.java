d280ee52ef5162341474de7cbb2497ce87074e87
Srivalasalan <manikandan85334@gmail.com>
2026-07-13 12:06:32 +0530

    Added Validations

---
+++ src/main/java/com/notes/dto/AuthResponse.java
@@ -0,0 +1,4 @@
+package com.notes.dto;
+
+public class AuthResponse {
+}
--- src/main/java/com/notes/entity/User.java
+++ src/main/java/com/notes/entity/User.java
@@ -17,8 +17,11 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false,unique = true)
     private String username;
+    @Column(nullable = false,unique = true)
     private String email;
+    @Column(nullable = false)
     private String password;
 
 }
