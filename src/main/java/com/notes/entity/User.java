78a71fea67ec7efeb905eb3c2db7e5088c17bf83
Srivalasalan <manikandan85334@gmail.com>
2026-07-13 11:59:13 +0530

    Initial Commit

--- src/main/java/com/notes/entity/User.java
+++ src/main/java/com/notes/entity/User.java
@@ -1,4 +1,23 @@
 package com.notes.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
 }
