c6384c26a3717ec8e2676cd65642e3f897bd94bb
Srivalasalan <manikandan85334@gmail.com>
2026-07-14 11:01:16 +0530

    Field Injection

--- src/main/java/com/notes/util/JwtUtil.java
+++ src/main/java/com/notes/util/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -12,8 +13,11 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
+    @Value("${jwt.secret}")
     private String secret;
+    @Value("${jwt.access-token-expiration}")
     private Long expiration;
+    @Value("${jwt.access-token-expiration}")
     private Long refreshExpiration;
 
 
